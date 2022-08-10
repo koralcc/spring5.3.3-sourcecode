801d74534acec2d027f62b84ea7191badb40dfe8
koralwins <warehousecjy@163.com>
2022-08-10 23:59:48 +0800

    init project

---
+++ spring-chenjianyun/src/main/java/com/koral/spring/config/CjyConfig.java
@@ -0,0 +1,15 @@
+package com.koral.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * spring
+ *
+ * @author koral
+ * @date 2022/8/10 12:06
+ */
+@Configuration
+@ComponentScan("com.koral")
+public class CjyConfig {
+}
